c534b975734f971cde6e47fc38a6d257cc60f965
temcray <ejamison06@gmail.com>
2025-07-30 20:57:54 -0500

    my-react-store-107

--- my-react-store/src/services/DataService.js
+++ my-react-store/src/services/DataService.js
@@ -3,7 +3,7 @@ const products = [
         _id: 1, //the id most Eq
         title: "Headphone",
         price: 60.00,
-        image: "https://th.bing.com/th/id/OIP.1lGKhbTN9MvGsH5AmQrSawHaHa?w=192&h=192&c=7&r=0&o=7&dpr=1.8&pid=1.7&rm=3",
+        image: "Headphone.jpeg",
         category: "Electronics"
     },
 
@@ -11,7 +11,7 @@ const products = [
         _id: 2, //the id most Eq
         title: "Cellphone",
         price: 160.00,
-        image: "https://th.bing.com/th/id/OIP.HxCUDuhXiUIYR73NuIvIvQHaEp?w=243&h=180&c=7&r=0&o=7&dpr=1.8&pid=1.7&rm=3",
+        image: "Cellphone.jpeg",
         category: "Electronics"
     },
 
@@ -19,7 +19,7 @@ const products = [
         _id: 3, //the id most Eq
         title: "Tablet",
         price: 90.00,
-        image: "https://th.bing.com/th/id/OIP.fRGEyXvRKAEi7Y0a5rf1LQHaEo?w=271&h=180&c=7&r=0&o=7&dpr=1.8&pid=1.7&rm=3",
+        image: "Tablet.jpeg",
         category: "Electronics"
     },
 
@@ -27,7 +27,7 @@ const products = [
         _id: 4, //the id most Eq
         title: "Computer",
         price: 689.00,
-        image: "https://th.bing.com/th/id/OIP.SN51uhQK2XJr3po6hRgDhQHaEi?w=280&h=180&c=7&r=0&o=7&dpr=1.8&pid=1.7&rm=3",
+        image: "Computer.jpeg",
         category: "Electronics"
     },
 
